23c35efc1b41ce6a52c6a7ff7d38e9cfa991fbf9
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-06-28 23:32:20 +0300

    Created a view for viewing post apis

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .serializers import PostSerializer
+from rest_framework import generics
+from .models import Post
 
 # Create your views here.
+class PostList(generics.ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
